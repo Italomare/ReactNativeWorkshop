7cbd0614e61929910b79e57b233d38af0620653c
Josh Pagley <jdpagley@yahoo.com>
2017-01-22 19:48:45 -0500

    Fixed the app file

--- index.ios.js
+++ index.ios.js
@@ -1,11 +1,57 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class ReactNativeWorkshop extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          {/*Change text below*/}
+          Welcome to React Native Workshop!
+        </Text>
+        <Text style={styles.instructions}>
+          Presented by Brandy Bergh and Josh Pagley
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
+ * Step 2:
+ * - Remove component and styles above
+ * - Import App file
+ * - Add App to AppRegister
  */
-
-import React from 'react';
-import { AppRegistry } from 'react-native';
-import App from './src/App'
-
-AppRegistry.registerComponent('ReactNativeWorkshop', () => App);
+AppRegistry.registerComponent('ReactNativeWorkshop', () => ReactNativeWorkshop);

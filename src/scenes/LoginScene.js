9c838cdb5c92e1893ac89a4f3fff9382cff4e6f1
Brandy Bergh <brandy@prpl.rs>
2017-01-24 20:47:33 -0500

    updated loginScene

--- src/scenes/LoginScene.js
+++ src/scenes/LoginScene.js
@@ -5,8 +5,36 @@ import LoginContainer from '../containers/LoginContainer';
 
 /**
  * Step 3:
- * - Create LoginScene component
+ * - Delete Text and Button component along with their styles
  * - Pass LoginContainer to view
- * - Style component
- * - Export LoginScene
  */
+
+class LoginScene extends Component{
+
+  render(){
+    return(
+      <View style={styles.container}>
+        <Text style={styles.text}>Login Scene</Text>
+        <Button onPress={Actions.tabbar} style={styles.button}>Login</Button>
+      </View>
+    )
+  }
+
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent: 'center',
+  },
+  text:{
+    color:'#05668D'
+  },
+  button: {
+    color:'#05668D',
+  }
+})
+
+
+
+export default LoginScene;

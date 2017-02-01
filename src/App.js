39cf3f758ef55834165edbded943510409aeb7d5
Josh Pagley <jdpagley@gmail.com>
2017-02-01 06:41:20 -0500

    Updated directory path

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const store = configureStore({});
 /*
  * TabBar Icons
  */
-import TabIcon from './components/nav/Tab';
+import TabIcon from './components/Nav/Tab';
 
 const App = () => {
   return(

54ce7515193515ab634b9694c11259c8a6ed2ba4
Josh Pagley <jdpagley@yahoo.com>
2017-02-01 06:01:14 -0500

    RouteActions by default

--- src/scenes/LoginScene.js
+++ src/scenes/LoginScene.js
@@ -3,12 +3,12 @@ import {View, Text, StyleSheet} from 'react-native';
 import Button from 'react-native-button';
 
 //Actions is provided by the router for dynamic routing
-import { Actions } from 'react-native-router-flux';
+import { Actions as RouteActions } from 'react-native-router-flux';
 
 /**
  * Step 2:
  * - Define Login Scene
- * - Add button to scene that navigates to the tabbar via dynamic routing (Actions)
+ * - Add button to scene that navigates to the tabbar via dynamic routing (RouteActions)
  * - Style Component
  * - export 
  */

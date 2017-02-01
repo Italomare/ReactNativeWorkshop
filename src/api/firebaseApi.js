9981b32cd4b488c170587e6cb91a27926328d9db
Josh Pagley <jdpagley@gmail.com>
2017-02-01 10:12:06 -0500

    Updated firebase credentials

--- src/api/firebaseApi.js
+++ src/api/firebaseApi.js
@@ -2,12 +2,14 @@ import * as firebase from 'firebase';
 
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyCxNUhNML2uw_NCEGiSQCHBgAaux7E5cTU",
-  authDomain: "reactnativeworkshop.firebaseapp.com",
-  databaseURL: "https://reactnativeworkshop.firebaseio.com",
-  storageBucket: "reactnativeworkshop.appspot.com",
-  messagingSenderId: "659877549359"
+  apiKey: "AIzaSyBWuB5XLygcYHgPfgmvK9cqTXgQoF9gM3c",
+  authDomain: "react-native-workshop-toronto.firebaseapp.com",
+  databaseURL: "https://react-native-workshop-toronto.firebaseio.com",
+  storageBucket: "react-native-workshop-toronto.appspot.com",
+  messagingSenderId: "1060807346956"
 };
+
+
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
